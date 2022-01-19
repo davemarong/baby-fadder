77b217b8956cdadc1b23f539b81c0c8c096bbc04
davemarong <kmarong@hotmail.com>
2022-01-19 09:42:20 +0100

    Change webpack config to use styled components with material ui

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system",
+  // "@mui/icons-material",
+]);
+
+module.exports = withTM({
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+    return config;
+  },
+});
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
+module.exports = nextConfig;
